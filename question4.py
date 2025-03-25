259eb67deff4fd5fd496263c7f6cc7e5c7e2f8cd
mjh <timmao955@gmail.com>
2025-03-25 23:26:23 +1100

    写完了question5

--- question4.py
+++ question4.py
@@ -10,10 +10,11 @@ def plot_graph(N):
     plt.grid(True)
     plt.show()
 
-def maximum_s(N = 10000):
+def maximum_s(N):
     p = 1 / N
     S_max = N * p * (1 - p) ** (N - 1)
     print(f"The maximum value of S ≈ {S_max:.6f}")
 
-plot_graph(10)
-maximum_s(1000)
+if __name__ == "__main__":
+    plot_graph(10)
+    maximum_s(10000)
